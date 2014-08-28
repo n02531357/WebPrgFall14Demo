8f8e9248b8eb06821081093b20537900c3a9fdc6
unknown <n02531357@LC112PC02.active.newpaltz.edu>
2014-08-28 10:37:30 -0400

    First Commit in Aptana Created an index page.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>index</title>
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>First File</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by n02531357
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
